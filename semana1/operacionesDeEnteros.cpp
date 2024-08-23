07f26fa0b3c7abf763bbfd5503060af18648496f
ASISTENCIAS <ASISTENCIAS@UCSP.AULAB>
2024-08-23 08:23:03 -0500

    Hello World y Ops de Enteros :)

---
+++ semana1/operacionesDeEnteros.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+    int var1 = 100;
+    int var2 = 20;
+
+    std::cout << var1 + var2 << std::endl; // 120
+    std::cout << var1 - var2 << std::endl; // 80
+    std::cout << var1 * var2 << std::endl; // 2000
+    std::cout << var1 / var2 << std::endl; // 5
+    std::cout << var1 % var2 << std::endl; // 0
+
+    return 0;
+}
